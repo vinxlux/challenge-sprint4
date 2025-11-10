9647249beb70bb818486c2decb741505a43e89ce
vinxlux <vinx.lux@gmail.com>
2025-11-09 23:27:51 -0300

    Arquivos do vercel atualizados

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
